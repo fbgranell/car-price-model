c9c261ddaaf5d83a5cfff25864ac02d4208bf2cc
fbgranell <fbgranell@gmail.com>
2026-07-01 23:43:41 +0200

    feat(frontend): add home and predict pages with routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import AppShell from './components/layout/AppShell'
+import HomePage from './pages/HomePage'
+import PredictPage from './pages/PredictPage'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
+export default function App() {
+  const location = useLocation()
+  return (
+    <AppShell>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/predict" element={<PredictPage />} />
+        </Routes>
+      </AnimatePresence>
+    </AppShell>
+  )
+}
